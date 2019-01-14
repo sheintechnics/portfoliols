14c57814fa3acaf60ad24239a78ffce124058123
Serg Kurnikov <kursgsg@gmail.com>
2019-01-14 11:36:27 +0300

    parallaxMouse_finish

---
+++ src/assets/scripts/modules/parallacsMouse.js
@@ -0,0 +1,16 @@
+const parallaxContainer = document.querySelector('.parallax');
+const layers = Array.from(parallaxContainer.children);
+
+const moveLayers = e => {
+	const initialX = (window.innerWidth/2) - e.pageX;
+	const initialY = (window.innerHeight/2) - e.pageY;
+
+	layers.forEach((layer, i) => {
+		const divider = i/150;
+		const positionX = initialX*divider;
+		const positionY = initialY*divider;
+		layer.style.transform = `translate(${positionX}px, ${positionY}px)`;
+	});
+} 
+
+window.addEventListener('mousemove', moveLayers)
